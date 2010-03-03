e6024fb00b97c136d4dcb76c8253e3179c9ba322
Cyril Robert <Cyril Robert crobert@inverse.ca>
2010-03-03 09:51:36 -0500

    Ajout des données initiales pour les catégories

---
+++ src/sigma.www/sigma/www/sql/categorie.sql
@@ -0,0 +1,11 @@
+INSERT INTO `www_categorie` (`id`, `nom`, `poste_id`, `actif`, `exception_possible`, `renouvellement_possible`, `renouvellement_nb_max`, `age_max`, `statut_etudiant_obligatoire`, `mobilite_duree_mois_min`, `mobilite_duree_mois_max`, `origine_etabl_membre_obligatoire`, `accueil_etabl_membre_obligatoire`, `diplome_duree_validite_obligatoire`, `diplome_duree_validite_min`, `alternance_obligatoire`, `alternance_duree_min_mois`, `niveau_etude_id`) VALUES
+(1, 'Bourses Eugen Ionescu - doctorales', 59, 1, 1, 1, 3, 70, 0, 3, 10, 1, 1, 0, NULL, 0, NULL, NULL),
+(2, 'Bourse de stage culturel', 132, 1, 0, 0, NULL, 90, 1, 1, 2, 1, 1, 0, NULL, 0, NULL, NULL),
+(3, 'Bourse de stage professionnel', 138, 1, 1, 0, NULL, 90, 1, 1, 4, 1, 0, 0, NULL, 0, NULL, NULL),
+(4, 'Mission d''appui et d''enseignement', 93, 1, 1, 0, NULL, 90, 0, 1, 4, 1, 1, 0, NULL, 0, NULL, NULL),
+(5, 'Bourse de doctorat', 59, 1, 0, 1, 2, 90, 1, 3, 10, 1, 1, 0, NULL, 1, 3, NULL),
+(6, 'Mobilité de réseaux scientifiques et associatifs', 19, 1, 0, 0, NULL, 90, 0, 1, 1, 1, 0, 0, NULL, 0, NULL, NULL),
+(7, 'Bourse de formation initiale', 64, 1, 0, 0, NULL, 90, 1, 3, 10, 1, 1, 0, NULL, 0, NULL, NULL),
+(8, 'Bourse de perfectionnement en formation', 104, 1, 1, 0, NULL, 90, 0, 1, 4, 1, 1, 0, NULL, 0, NULL, NULL),
+(9, 'Bourse de perfectionnement en recherche (hors post-doc)', 111, 1, 1, 0, NULL, 90, 0, 1, 4, 1, 1, 0, NULL, 0, NULL, NULL),
+(10, 'Bourse de post-doctorat', 110, 1, 0, 0, NULL, 90, 0, 3, 10, 1, 1, 1, 2, 0, NULL, NULL);
