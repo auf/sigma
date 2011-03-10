5c1cfe99de9c17a8bf880beee39be26c0062b65a
Olivier Larchevêque <olivier.larcheveque@auf.org>
2011-03-10 11:05:54 -0500

    sigma bootstrap

---
+++ project/settings.py
@@ -0,0 +1,84 @@
+# -*- encoding: utf-8 -*-
+
+import os
+import socket
+from conf import *
+
+PROJET_TITRE = "SIGMA"
+
+# Rapports d'erreurs
+EMAIL_SUBJECT_PREFIX = '[SIGMA - %s] ' % socket.gethostname()
+ADMINS = (
+    ('Équipe ARI-SI', 'developpeurs@ca.auf.org'),
+)
+
+MANAGERS = ADMINS
+
+TIME_ZONE = 'Canada/Montreal'
+
+LANGUAGE_CODE = 'fr-ca'
+
+# Absolute path to the directory that holds media.
+# Example: "/home/media/media.lawrence.com/"
+MEDIA_ROOT = os.path.join(os.path.dirname(__file__), 'media')
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash if there is a path component (optional in other cases).
+# Examples: "http://media.lawrence.com", "http://example.com/media/"
+MEDIA_URL = '/media/'
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/media/django/'
+
+# Don't share this with anybody.
+SECRET_KEY = 'wbt#ie2bktr9c7o%4cgr&9@%!3ik3#f6lh#6k@fb=l5&*ndr-m'
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.middleware.doc.XViewMiddleware',
+)
+
+ROOT_URLCONF = 'project.urls'
+
+
+INSTALLED_APPS = (
+    'auf.django.skin',
+    'admin_tools',
+    'admin_tools.theming',
+    'admin_tools.menu',
+    'admin_tools.dashboard',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.admin',
+    'south',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
+    'auf.django.skin.context_processors.auf',
+)
+
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.load_template_source',
+    'django.template.loaders.app_directories.load_template_source',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), "templates"),
+)
+
+SOUTH_TESTS_MIGRATE = False
+
+ADMIN_TOOLS_INDEX_DASHBOARD = 'project.dashboard.CustomIndexDashboard'
+
---
+++ project/urls.py
@@ -0,0 +1,26 @@
+# -*- encoding: utf-8 -*
+from django.conf.urls.defaults import patterns, include, handler500, url
+from django.conf import settings
+from django.contrib import admin
+
+admin.autodiscover()
+
+handler500 # Pyflakes
+
+urlpatterns = patterns(
+    '',
+    ######## page d'accueil de demo ######
+    (r'^$', 'auf.django.skin.views.demo'),
+    ######################################
+    url(r'^admin_tools/', include('admin_tools.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'^connexion/$', 'django.contrib.auth.views.login'),
+    (r'^deconnexion/$', 'django.contrib.auth.views.logout'),
+
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve',
+         {'document_root': settings.MEDIA_ROOT}),
+    )
