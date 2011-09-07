8bf3ee7f499cb42ef718487d3a14302601ed9e61
Olivier Larchevêque <olivier.larcheveque@auf.org>
2011-09-07 10:06:34 -0400

    aide

--- project/wcs/management/commands/wcs.py
+++ project/wcs/management/commands/wcs.py
@@ -4,12 +4,28 @@ from django.core.management.base import BaseCommand, CommandError
 from project.wcs import tools as cmd
 
 class Command(BaseCommand):
+    """
 
+    Aide
+    ====
+
+    Appel
+    -----
+    * appel liste
+    * appel clear
+    * appel dossiers <appel_id>
+    * appel dossier <appel_id> <dossier_id>
+    * appel test <appel_id>
+
+    """
     def handle(self, *args, **options):
         """
         Dispatcher de commandes
         """
-        classname, method = args[0:2]
-        classname = classname.title()
-        instance = getattr(cmd, classname)()
-        getattr(instance, method)(*args[2:])
+        try:
+            classname, method = args[0:2]
+            classname = classname.title()
+            instance = getattr(cmd, classname)()
+            getattr(instance, method)(*args[2:])
+        except:
+            print self.__doc__
