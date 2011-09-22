ba04c2e53ceec1f6a1ff3346b426f128f4385419
Eric Mc Sween <eric.mcsween@auf.org>
2011-09-22 16:23:10 -0400

    [#913] Fiche de suivi du boursier

--- project/suivi/admin.py
+++ project/suivi/admin.py
@@ -1,8 +1,11 @@
 # -*- encoding: utf-8 -*-
 
+from django.conf.urls.defaults import patterns, url
 from django.contrib import admin
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm, ValidationError
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from django.utils.html import conditional_escape
 
 from sigma.workflow import DOSSIER_ETAT_BOURSIER
@@ -19,36 +22,26 @@ class BoursierAdminForm(ModelForm):
         boursier = self.instance
         if boursier and code_operation:
             code_budgetaire = boursier.dossier.appel.code_budgetaire
+
+            # Vérifier le format du code opération
             if not code_operation.startswith(code_budgetaire) or \
                not code_operation.endswith('L'):
                 raise ValidationError(
                     u"Code d'opération invalide: il devrait avoir la forme %sXXXL" % 
                     code_budgetaire
                 )
+            
         return code_operation
 
 
 class BoursierAdmin(admin.ModelAdmin):
 
-    # Champs calculés
-
-    def dossier_link(boursier):
-        return '<a href="%s">%s</a>' % (
-            reverse('admin:sigma_dossier_change', args=(boursier.dossier.pk,)),
-            conditional_escape(boursier.dossier)
-        )
-    dossier_link.allow_tags = True
-    dossier_link.short_description = 'Dossier de candidature'
-
     # Configuration
 
-    # XXX: La seule façon de faire référence à la méthode 'dossier_link' dans
-    # l'objet admin ici est de passer la méthode elle-même. C'est pour ça que
-    # sa définition est placée plus haut. Bug Django?
-    readonly_fields = ('nom_complet', dossier_link)
-    fields = ('nom_complet', dossier_link, 'code_operation')
+    list_display = ('nom_complet', 'code_operation', 'field_actions')
     form = BoursierAdminForm
-    list_display = ('nom_complet', 'code_operation')
+    readonly_fields = ('nom_complet', 'field_dossier')
+    fields = ('nom_complet', 'field_dossier', 'code_operation')
 
     # Queryset
 
@@ -57,5 +50,44 @@ class BoursierAdmin(admin.ModelAdmin):
         # pas qu'ils sont boursiers.
         qs = super(BoursierAdmin, self).queryset(request)
         return qs.filter(dossier__etat=DOSSIER_ETAT_BOURSIER)
+
+    # Champs calculés
+
+    def field_dossier(self, boursier):
+        return '<a href="%s">%s</a>' % (
+            reverse('admin:sigma_dossier_change', args=(boursier.dossier.pk,)),
+            conditional_escape(boursier.dossier)
+        )
+    field_dossier.allow_tags = True
+    field_dossier.short_description = 'Dossier de candidature'
+
+    def field_actions(self, obj):
+        return '<a href="%s">Suivi</a>' % reverse('admin:suivi_boursier_suivi', args=(obj.pk,))
+    field_actions.short_description = ''
+    field_actions.allow_tags = True
+
+    # Vues additionnelles
+
+    def get_urls(self):
+        admin_urls = super(BoursierAdmin, self).get_urls()
+        additional_urls = patterns('',
+            url(r'^(\d+)/suivi/$', self.admin_site.admin_view(self.view_suivi),
+                name='suivi_boursier_suivi'),
+        )
+        return additional_urls + admin_urls
+
+    def view_suivi(self, request, id):
+        boursier = Boursier.objects.get(pk=id)
+        ecritures = boursier.ecritures_coda.order_by('pcg', 'date')
+        return render_to_response('admin/suivi/boursier/suivi.html', {
+            'boursier': boursier,
+            'ecritures': ecritures
+        }, context_instance=RequestContext(request))
+
+    # Permissions
+
+    def has_add_permission(self, request):
+        # L'ajout de boursier n'est jamais manuel
+        return False
 
 admin.site.register(Boursier, BoursierAdmin)
--- project/suivi/models.py
+++ project/suivi/models.py
@@ -9,11 +9,11 @@ from sigma.workflow import DOSSIER_ETAT_BOURSIER
 
 class Boursier(models.Model):
     """La fiche de suivi d'un boursier."""
-    dossier = models.OneToOneField(Dossier, verbose_name='Dossier de candidature', 
+    dossier = models.OneToOneField(Dossier, verbose_name='dossier de candidature',
                                    related_name='boursier', primary_key=True,
                                    editable=False)
-    code_operation = models.CharField(max_length=11, verbose_name="Code d'opération CODA",
-                                      blank=True, default='')
+    code_operation = models.CharField(max_length=11, verbose_name="code d'opération CODA",
+                                      blank=True, unique=True, null=True, db_index=True)
 
     class Meta:
         verbose_name = 'boursier'
@@ -34,8 +34,35 @@ class Boursier(models.Model):
         return self.prenom + ' ' + self.nom
     nom_complet.short_description = 'Nom'
 
-
 def dossier_post_save(sender, instance=None, **kwargs):
     if instance and instance.etat == DOSSIER_ETAT_BOURSIER:
         Boursier.objects.get_or_create(dossier=instance)
 post_save.connect(dossier_post_save, sender=Dossier)
+
+
+class EcritureBoursier(models.Model):
+    """Une écriture CODA relative à un boursier."""
+    boursier = models.ForeignKey(Boursier, db_column='code',
+                                 verbose_name='boursier',
+                                 related_name='ecritures_coda',
+                                 to_field='code_operation')
+    pcg = models.CharField(max_length=72)
+    nom_pcg = models.CharField(max_length=36)
+    code = models.CharField(max_length=12, db_column='doccode')
+    numero = models.CharField(max_length=12, db_column='docnum')
+    annee = models.IntegerField(db_column='yr')
+    periode = models.IntegerField(db_column='period')
+    date = models.DateTimeField(db_column='date_doc')
+    description = models.CharField(max_length=36, db_column='descr_ligne')
+    montant_eur = models.DecimalField(max_digits=12, decimal_places=2, db_column='montanteur')
+    debit_credit = models.CharField(max_length=6, db_column='dc')
+    implantation = models.CharField(max_length=32, db_column='imp_payeur')
+    employe = models.CharField(max_length=36, db_column='salarie')
+    paiement = models.CharField(max_length=23, db_column='statut_paiement')
+
+    class Meta:
+        db_table = 'coda_ecriture_boursier'
+        managed = False
+        ordering = ['date']
+        verbose_name = 'écriture CODA'
+        verbose_name_plural = 'écritures CODA'
