4ccb17cbd047dfc570d1f742e4e3e56b95c1eb0f
Eric Mc Sween <eric.mcsween@auf.org>
2011-10-04 18:34:05 -0400

    [#2196] Collapse sur les regroupements d'écritures

--- project/media/js/suivi-boursier.js
+++ project/media/js/suivi-boursier.js
@@ -25,8 +25,39 @@
         });
     }
 
+    function setup_tbody_toggle() {
+        $('tbody').each(function() {
+            var $header = $(this).children('tr').first().children('th').first();
+            if ($header.length > 0) {
+                var $collapse_link = $('<a href="#"></a>');
+                $header.append('(').append($collapse_link).append(')');
+                $collapse_link.click(collapse_link_clicked);
+                $collapse_link.each(collapse_link_clicked);
+            }
+        });
+    }
+
+    function collapse_link_clicked() {
+        var $rows = $(this).closest('tbody').find('tr:not(:first-child)');
+        if ($rows.is(':visible')) {
+            $rows.hide();
+            $(this).text('Détails');
+        }
+        else {
+            $rows.show();
+            $(this).text('Cacher les détails');
+        }
+        $('#keep-thead-visible').remove();
+        keep_thead_visible();
+        return false;
+    }
+
+
+
     $(document).ready(function() {
         $(window).scroll(keep_thead_visible);
+        setup_tbody_toggle();
     });
+
 })(jQuery);
     
