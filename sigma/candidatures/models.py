7cfb0b4854731421b6c79b41645e10f37f312864
Davin Baragiotta <davin.baragiotta@auf.org>
2012-10-30 13:15:54 -0400

    #4028 : retrait default sur montants

--- sigma/candidatures/models.py
+++ sigma/candidatures/models.py
@@ -163,39 +163,39 @@ class Appel(MetaModel, models.Model):
         u"Barème", max_length=32, choices=BAREME, blank=True
     )
     montant_mensuel_origine_sud = models.IntegerField(
-        u"Montant mensuel pays origine Sud", default=0,
+        u"Montant mensuel pays origine Sud", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     montant_mensuel_origine_nord = models.IntegerField(
-        u"Montant mensuel pays origine Nord", default=0,
+        u"Montant mensuel pays origine Nord", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     montant_mensuel_accueil_sud = models.IntegerField(
-        u"Montant mensuel pays accueil Sud", default=0,
+        u"Montant mensuel pays accueil Sud", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     montant_mensuel_accueil_nord = models.IntegerField(
-        u"Montant mensuel pays accueil Nord", default=0,
+        u"Montant mensuel pays accueil Nord", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     prime_installation_sud = models.IntegerField(
-        u"Prime d'installation (pays du Sud)", default=0,
+        u"Prime d'installation (pays du Sud)", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     prime_installation_nord = models.IntegerField(
-        u"Prime d'installation (pays du Nord)", default=0,
+        u"Prime d'installation (pays du Nord)", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     montant_perdiem_sud = models.IntegerField(
-        u"Montant jour (perdiem) pays Sud ", default=0,
+        u"Montant jour (perdiem) pays Sud ", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     montant_perdiem_nord = models.IntegerField(
-        u"Montant jour (perdiem) pays Nord", default=0,
+        u"Montant jour (perdiem) pays Nord", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     montant_allocation_unique = models.IntegerField(
-        u"Montant allocation unique", default=0,
+        u"Montant allocation unique", 
         help_text=u"Indiquer 0 dans les champs non-pertinents",
     )
     conformites = models.ManyToManyField(
