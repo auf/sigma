49c2a324b0163c03f87d179ec932c8a29402747c
Olivier Larchevêque <olivier.larcheveque@gmail.com>
2012-10-30 00:05:54 -0400

    #4282

--- sigma/urls.py
+++ sigma/urls.py
@@ -1,14 +1,34 @@
 # -*- encoding: utf-8 -*
+
 from django.conf.urls.defaults import patterns, include, url
 from django.conf.urls.defaults import handler500, handler404  # NOQA
+from django.views.generic.simple import direct_to_template
+
+from django.conf import settings
+
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^', include(admin.site.urls)),
     url(r'^candidatures/', include('sigma.candidatures.urls')),
     url(r'^admin_tools/', include('admin_tools.urls')),
     url(r'^wcs/', include('sigma.wcs.urls')),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        '',
+        url(r'^500/$',  direct_to_template, {
+            'template': '500.html',
+            }),
+        url(r'^404/$',  direct_to_template, {
+            'template': '404.html',
+            }),
+    )
+    
+urlpatterns += patterns(
+    '',
+    url(r'^', include(admin.site.urls)),
 )
